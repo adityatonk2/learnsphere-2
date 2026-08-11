f22fa1ce004a94b9fec6a00552729fcaa743d3c3
FDC-innovation <webdevelopments@fastdataconnect.com>
2026-08-11 19:16:47 +0530

    Initial commit

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/aysuh/Downloads/learnsphere-technologies/app/layout.tsx
+// File: /Users/adityakumartonk/Desktop/ed-tech/ui/app/layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/aysuh/Downloads/learnsphere-technologies/app/page.tsx
+// File: /Users/adityakumartonk/Desktop/ed-tech/ui/app/page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
